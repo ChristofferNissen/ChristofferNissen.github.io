36b801b804078174036d25d2d462cf7d7c122705
Christoffer Nissen <christoffer.nissen@gmail.com>
2018-02-13 16:44:10 +0100

    Removed +200 from dimensions

--- Videobaggrund/Videobakgrund.js
+++ Videobaggrund/Videobakgrund.js
@@ -28,8 +28,8 @@ function onPlayerStateChange(e) {
 }
 
 function vidRescale(){
-  var w = $(window).width()+200;
-  var h = $(window).height()+200;
+  var w = $(window).width();
+  var h = $(window).height();
 
   if (w/h > 16/9){
     tv.setSize(w, w/16*9);
